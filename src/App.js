da61e7f389bb3fae2b4745f297bb248bdad80fb9
ahmadfaqih796 <ahmadfaqih796@gmail.com>
2022-11-22 20:51:49 +0700

    routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Example from "./materi/1-useState/Example";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Example/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/materi/1-useState/Example.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Example = () => {
+	return (
+		<div>Hello world</div>
+	)
+}
+
+export default Example
