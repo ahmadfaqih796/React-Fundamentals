2465541c9e6575dfdfcc3d89cf5f801d3329c12d
ahmadfaqih796 <ahmadfaqih796@gmail.com>
2022-11-24 14:36:47 +0700

    parent to child

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Example from "./materi/1-Styling/Example";
+import Parent from "./materi/2-Child-to-Parent/Parent";
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route index element={<Example/>} />
+				<Route path="/styling" element={<Example/>} />
+				<Route path="/child-to-parent" element={<Parent/>} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/materi/Beranda.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Beranda = () => {
+	return (
+		<div>Beranda</div>
+	)
+}
+
+export default Beranda
